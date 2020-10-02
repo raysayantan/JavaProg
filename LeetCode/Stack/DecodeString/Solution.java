721ae30ad447ae2165e41a59ddb5d7fc5bfa7918
Sayantan Ray <ray.sayantan@gmail.com>
2020-10-02 19:10:46 +0530

    Create Solution.java

---
+++ LeetCode/Stack/DecodeString/Solution.java
@@ -0,0 +1,71 @@
+/*
+Given an encoded string, return its decoded string.
+
+The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times. Note that k is guaranteed to be a positive integer.
+
+You may assume that the input string is always valid; No extra white spaces, square brackets are well-formed, etc.
+
+Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k. For example, there won't be input like 3a or 2[4].
+
+ 
+
+Example 1:
+
+Input: s = "3[a]2[bc]"
+Output: "aaabcbc"
+Example 2:
+
+Input: s = "3[a2[c]]"
+Output: "accaccacc"
+Example 3:
+
+Input: s = "2[abc]3[cd]ef"
+Output: "abcabccdcdcdef"
+Example 4:
+
+Input: s = "abc3[cd]xyz"
+Output: "abccdcdcdxyz"
+*/
+class Solution {
+    public String decodeString(String s) {
+        Stack<String> st = new Stack<>();
+        int len = s.length();
+        String res = "";
+        int idx = len - 1;
+        while(idx >= 0){
+            if(s.charAt(idx) == '['){
+                //pop the stack till we get closing ']'
+                String str = "";
+                while(!st.empty() && !st.peek().equals("]")){
+                    str += st.pop();
+                }
+                st.pop();
+                int freq = 0;
+                String number = "";
+                idx--;
+                while(idx >= 0 && s.charAt(idx) >= '0' && s.charAt(idx) <= '9'){
+                    number = s.charAt(idx) + number;
+                    idx--;
+                }
+                
+                if(number != ""){
+                    freq = Integer.parseInt(number);
+                }
+                String word = "";
+                for(int count = 0; count < freq; count++){
+                    word += str;
+                }
+                st.push(word);
+            } else {
+                st.push(Character.toString(s.charAt(idx)));
+                idx--;
+            }
+        }
+        
+        while(!st.empty()){
+            res += st.pop();
+        }
+        
+        return res;
+    }
+}
