5a4edd23ea9fe4980c9ac3c3d6d0aabddd824e18
Sayantan Ray <ray.sayantan@gmail.com>
2021-03-21 06:52:12 +0530

    Create Solution.java

---
+++ LeetCode/Tree/InorderTraversal/Solution.java
@@ -0,0 +1,79 @@
+/*
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+Example 4:
+
+
+Input: root = [1,2]
+Output: [2,1]
+Example 5:
+
+
+Input: root = [1,null,2]
+Output: [1,2]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+ 
+
+Follow up:
+
+Recursive solution is trivial, could you do it iteratively?
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    Stack<TreeNode> s = new Stack<>();
+    private void construct(TreeNode root){
+        TreeNode next = root;
+        while(next != null){
+            s.push(next);
+            next = next.left;
+        }
+    }
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if(root == null)
+            return res;
+        construct(root);
+        
+        while(!s.isEmpty()){
+            TreeNode top = s.pop();
+            res.add(top.val);
+            construct(top.right);
+        }
+        
+        return res;
+    }
+}
