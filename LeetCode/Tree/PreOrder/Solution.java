eb7540bebd0aaf800bb7751b89d98256e8b2b583
Sayantan Ray <ray.sayantan@gmail.com>
2021-03-23 06:49:32 +0530

    Create Solution.java

---
+++ LeetCode/Tree/PreOrder/Solution.java
@@ -0,0 +1,75 @@
+/*
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,null,2,3]
+Output: [1,2,3]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+Example 4:
+
+
+Input: root = [1,2]
+Output: [1,2]
+Example 5:
+
+
+Input: root = [1,null,2]
+Output: [1,2]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        Stack<TreeNode> st = new Stack<>();
+        List<Integer> res = new ArrayList<>();
+        
+        if(root == null){
+            return res;
+        }
+        
+        st.push(root);
+        while(!st.isEmpty()){
+            TreeNode top = st.pop();
+            res.add(top.val);
+            if(top.right != null){
+                st.push(top.right);
+            }
+            
+            if(top.left != null){
+                st.push(top.left);
+            }
+        }
+        
+        return res;
+    }
+}
