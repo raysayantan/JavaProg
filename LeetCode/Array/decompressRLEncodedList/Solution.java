f3a6b7639ae36dd6360e74ab0854ebd1e8e19f3b
Sayantan Ray <ray.sayantan@gmail.com>
2021-06-05 09:10:13 +0530

    Create Solution.java

---
+++ LeetCode/Array/decompressRLEncodedList/Solution.java
@@ -0,0 +1,51 @@
+/*
+We are given a list nums of integers representing a list compressed with run-length encoding.
+
+Consider each adjacent pair of elements [freq, val] = [nums[2*i], nums[2*i+1]] (with i >= 0).  For each such pair, there are freq elements with value val concatenated in a sublist. Concatenate all the sublists from left to right to generate the decompressed list.
+
+Return the decompressed list.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [2,4,4,4]
+Explanation: The first pair [1,2] means we have freq = 1 and val = 2 so we generate the array [2].
+The second pair [3,4] means we have freq = 3 and val = 4 so we generate [4,4,4].
+At the end the concatenation [2] + [4,4,4] is [2,4,4,4].
+Example 2:
+
+Input: nums = [1,1,2,3]
+Output: [1,3,3]
+ 
+
+Constraints:
+
+2 <= nums.length <= 100
+nums.length % 2 == 0
+1 <= nums[i] <= 100
+*/
+class Solution {
+    public int[] decompressRLElist(int[] nums) {
+        int len = 0;
+        for(int idx = 0; idx < nums.length; idx = idx + 2){
+            len += nums[idx];
+        }
+        
+        int[] res = new int[len];
+        int idx = 0;
+        int pos = 0;
+        while(idx < nums.length){
+            int freq = nums[idx];
+            int val = nums[idx + 1];
+            for(int i = 0; i < freq; i++){
+                res[pos++] = val;
+            }
+            
+            idx += 2;
+        }
+        
+        return res;
+    }
+}
