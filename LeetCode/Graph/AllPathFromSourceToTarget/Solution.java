f87b9fd81f4dc2017a071cd16bcedf819758ef4f
Sayantan Ray <ray.sayantan@gmail.com>
2021-06-17 07:22:55 +0530

    Create Solution.java

---
+++ LeetCode/Graph/AllPathFromSourceToTarget/Solution.java
@@ -0,0 +1,73 @@
+/*
+Given a directed acyclic graph (DAG) of n nodes labeled from 0 to n - 1, find all possible paths from node 0 to node n - 1, and return them in any order.
+
+The graph is given as follows: graph[i] is a list of all nodes you can visit from node i (i.e., there is a directed edge from node i to node graph[i][j]).
+
+ 
+
+Example 1:
+
+
+Input: graph = [[1,2],[3],[3],[]]
+Output: [[0,1,3],[0,2,3]]
+Explanation: There are two paths: 0 -> 1 -> 3 and 0 -> 2 -> 3.
+Example 2:
+
+
+Input: graph = [[4,3,1],[3,2,4],[3],[4],[]]
+Output: [[0,4],[0,3,4],[0,1,3,4],[0,1,2,3,4],[0,1,4]]
+Example 3:
+
+Input: graph = [[1],[]]
+Output: [[0,1]]
+Example 4:
+
+Input: graph = [[1,2,3],[2],[3],[]]
+Output: [[0,1,2,3],[0,2,3],[0,3]]
+Example 5:
+
+Input: graph = [[1,3],[2],[3],[]]
+Output: [[0,1,2,3],[0,3]]
+ 
+
+Constraints:
+
+n == graph.length
+2 <= n <= 15
+0 <= graph[i][j] < n
+graph[i][j] != i (i.e., there will be no self-loops).
+The input graph is guaranteed to be a DAG.
+*/
+class Solution {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> l = new ArrayList<>();
+        int n = graph.length;
+        boolean[] v = new boolean[n];
+        l.add(0);
+        
+        helper(graph, res, 0, n - 1, l, v);
+        
+        return res;
+    }
+    
+    private void helper(int[][] graph, List<List<Integer>> res, int node, int n, List<Integer> l, boolean[] v){
+        if(node == n){
+            List<Integer> temp = new ArrayList<>();
+            temp.addAll(l);
+            res.add(temp);
+            return;
+        }
+        
+        v[node] = true;
+        for(int idx = 0; idx < graph[node].length; idx++){
+            int next = graph[node][idx];
+            if(v[next] == false){
+                l.add(next);
+                helper(graph, res, next, n, l, v);
+                l.remove(l.size() - 1);
+            }
+        }
+        v[node] = false;
+    }
+}
