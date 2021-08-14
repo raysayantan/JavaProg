2d7e0256afb0689240ac48a386c61c9c4f41d1fc
Sayantan Ray <ray.sayantan@gmail.com>
2021-08-14 13:51:16 +0530

    Create Solution.java

---
+++ LeetCode/String/SortestWordDist/Solution.java
@@ -0,0 +1,43 @@
+/*
+Given an array of strings wordsDict and two different strings that already exist in the array word1 and word2, return the shortest distance between these two words in the list.
+
+ 
+
+Example 1:
+
+Input: wordsDict = ["practice", "makes", "perfect", "coding", "makes"], word1 = "coding", word2 = "practice"
+Output: 3
+Example 2:
+
+Input: wordsDict = ["practice", "makes", "perfect", "coding", "makes"], word1 = "makes", word2 = "coding"
+Output: 1
+ 
+
+Constraints:
+
+1 <= wordsDict.length <= 3 * 104
+1 <= wordsDict[i].length <= 10
+wordsDict[i] consists of lowercase English letters.
+word1 and word2 are in wordsDict.
+word1 != word2
+*/
+class Solution {
+    public int shortestDistance(String[] wordsDict, String word1, String word2) {
+        int idx1 = -1, idx2 = -1;
+        int mindist = wordsDict.length;
+        
+        for(int idx = 0; idx < wordsDict.length; idx++){
+            if(wordsDict[idx].equals(word1)){
+                idx1 = idx;
+            } else if(wordsDict[idx].equals(word2)){
+                idx2 = idx;
+            }
+            
+            if(idx1 != -1 && idx2 != -1){
+                mindist = Math.min(mindist, Math.abs(idx1 - idx2));
+            }
+        }
+        
+        return mindist;
+    }
+}
